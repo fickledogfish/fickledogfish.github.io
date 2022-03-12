f42cc01b75850449af02e7531c65e9ff98147ffa
Aníbal Vilela <anibal.vilela@modal.com.br>
2022-03-11 21:48:14 -0300

    Remover common-html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-import { HTMLInclude } from "./html-include.js";
-
-window.customElements.define("html-include", HTMLInclude);
-
 window.onload = () => {
 	document
 		.querySelectorAll("pre code[class^=\"language-\"]")
